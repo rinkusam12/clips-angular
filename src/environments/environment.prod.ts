a814fee06539b4b111ec5767a102cd50a5f8ef35
Rinku Samanta <rinkusamanta12@outlook.com>
2022-06-26 13:21:23 +0530

    Completed project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC3XgvC8wstscTHNfaooq8PrpdW2olxwa4',
+    authDomain: 'clip-7f8fd.firebaseapp.com',
+    projectId: 'clip-7f8fd',
+    storageBucket: 'clip-7f8fd.appspot.com',
+    messagingSenderId: '105762521300',
+    appId: '1:105762521300:web:3d6bc338a0f329062dbb41',
+  },
 };
